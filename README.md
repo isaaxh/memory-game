3ef034c4a701d4687a1334cea23067ab159498fe
Isaac Hussain <isaaclearns58@gmail.com>
2023-01-20 15:22:56 +0300

    Adds code for revealing the img

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 psuedo code:
 
+Random changing of cards
+
 - Make an array that has 6 indices
 - make a function that will give a random number between 0 and 6
 - make a loop and assign each one of those random numbers to each of the indices in the array
 - now assign each imgs to the indices in the array so they don't have same values
+
+Covering the cards
--- script.js
+++ script.js
@@ -7,24 +7,6 @@ const rabbit2 = document.querySelector('.rabbit-2');
 
 
 
-function changeCard() {
-    const randomArray = generateRandomArray();
-    const card1 = document.querySelector(`.card-${randomArray[0]}`);
-    const card2 = document.querySelector(`.card-${randomArray[1]}`);
-    const card3 = document.querySelector(`.card-${randomArray[2]}`);
-    const card4 = document.querySelector(`.card-${randomArray[3]}`);
-    const card5 = document.querySelector(`.card-${randomArray[4]}`);
-    const card6 = document.querySelector(`.card-${randomArray[5]}`);
-
-    card1.appendChild(cat1);
-    card2.appendChild(dog1);
-    card3.appendChild(rabbit1);
-    card4.appendChild(cat2);
-    card5.appendChild(dog2);
-    card6.appendChild(rabbit2);
-}
-
-
 function generateRandomNumber() {
     return Math.floor(Math.random() * 6) + 1;
 }
@@ -51,7 +33,28 @@ function generateRandomArray() {
     return randomArray;
 }
 
+function changeCard() {
+    const randomArray = generateRandomArray();
+    const card1 = document.querySelector(`.card-${randomArray[0]}`);
+    const card2 = document.querySelector(`.card-${randomArray[1]}`);
+    const card3 = document.querySelector(`.card-${randomArray[2]}`);
+    const card4 = document.querySelector(`.card-${randomArray[3]}`);
+    const card5 = document.querySelector(`.card-${randomArray[4]}`);
+    const card6 = document.querySelector(`.card-${randomArray[5]}`);
+
+    card1.appendChild(cat1);
+    card2.appendChild(dog1);
+    card3.appendChild(rabbit1);
+    card4.appendChild(cat2);
+    card5.appendChild(dog2);
+    card6.appendChild(rabbit2);
+}
+
+function revealImg(e) {
+    const img = e.target.querySelector('img');
+
+    img.style.display = 'block';
+}
 
 
-changeCard();
-// numIsPresent(,randomArray);
+// changeCard();
